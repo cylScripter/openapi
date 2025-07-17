b259141bb40e73f331f4174c312df8f8c7a42efc
cyl <2842871262@qq.com>
2025-07-17 11:46:50 +0800

    feat: sysetem 20250717114650

--- build.sh
+++ build.sh
@@ -8,16 +8,21 @@ fi
 
 
 
-# 检查是否提供了版本参数
-if [ -z "$2" ]; then
-  echo "请提供版本号，例如: ./build.sh system  v1.0.1 "
-  exit 1
-fi
+## 随机生成版本号
+function generate_random_version() {
+  echo "$(date +%Y%m%d%H%M%S)"
+   return
+}
+
+
+version=$(generate_random_version)
+
+echo "生成版本号: $version"
 
 kitex -module github.com/cylScripter/openapi -template-dir /Users/cyl/project/kitex/client/standard2 -gen-path ./ -I ~/project/idl $1.thrift
 
 git add .
-git commit -m "feat: $1 $2"
+git commit -m "feat: $1 $version"
 
 git tag $2
 git push origin $2
